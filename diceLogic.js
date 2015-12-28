f452c6da8d35109c12dfe7c60c1cb9d7a2965a44
Spencer Palmer <palmersp@spencerpalmer.com>
2015-12-27 21:32:33 -0800

    get number of Units

--- diceLogic.js
+++ diceLogic.js
@@ -1,13 +1,32 @@
 window.onload = function(e) {
+  // get number of units
+
+  var infantryRolls = [];
+  var artillaryRolls = [];
+  var tankRolls = [];
+
+  var infantryNum;
+  var artillaryNum;
+  var tankNum;
+
   document.querySelector('#infantryInput').onchange = function(e) {
-    document.querySelector('.infantryValue').innerHTML = e.target.value;
+    var num = document.querySelector('.infantryValue').innerHTML = e.target.value;
+    infantryNum = num;
+    console.log(infantryNum);
   }
   document.querySelector('#artillaryInput').onchange = function(e) {
-    document.querySelector('.artillaryValue').innerHTML = e.target.value;
+    var num = document.querySelector('.artillaryValue').innerHTML = e.target.value;
+    artillaryNum = num;
+    console.log(artillaryNum);
   }
   document.querySelector('#tankInput').onchange = function(e) {
-    document.querySelector('.tankValue').innerHTML = e.target.value;
+    var num = document.querySelector('.tankValue').innerHTML = e.target.value;
+    tankNum = num;
+    console.log(tankNum);
   }
 
+  // calculate dice rolls
+
+  // display dice number of hits
 
 }
